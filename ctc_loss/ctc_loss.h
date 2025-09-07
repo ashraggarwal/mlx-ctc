d3313d0f71998b578f29f66e4da0f621e3ea1859
ashraggarwal <62974975+ashraggarwal@users.noreply.github.com>
2025-09-06 17:10:49 -0700

    Update ctc_loss.h namespace fixes

--- ctc_loss/ctc_loss.h
+++ ctc_loss/ctc_loss.h
@@ -5,7 +5,9 @@
 #include "mlx/ops.h"
 #include "mlx/primitives.h"
 
-namespace mlx::core {
+namespace mx = mlx::core;
+
+namespace ctc_ext {
 
 /**
  *  The Connectionist Temporal Classification loss.
@@ -18,14 +20,14 @@ namespace mlx::core {
  *  Return: `(N)`, where `N = batch size`
  * 
  **/
-array ctc_loss(
+mx::array ctc_loss(
   /**
    *  The logarithmized probabilities of the outputs (e.g. obtained with `mlx::core::log_softmax`)
    *  of size `(T, N, C)`, where
    *  `T = input length`, `N = batch size`, and
    *  `C = number of classes` (including blank)
    */
-  const array& log_probs,
+  const mx::array& log_probs,
   /**
    *  Target sequences of size `(N, S)`, where
    *  `N = batch size` and `S = max target length`.
@@ -33,52 +35,52 @@ array ctc_loss(
    *  Target index cannot be blank (default=0).
    *  Targets are padded to the length of the longest sequence, and stacked.
    */
-  const array& targets,
+  const mx::array& targets,
   /**
    *  Lengths of the inputs of size `(N)`, where `N = batch size` (must each be <= `T`).
    *  Lengths are specified for each sequence to achieve masking under the assumption that
    *  sequences are padded to equal lengths.
    */
-  const array& input_lengths,
+  const mx::array& input_lengths,
   /**
    *  Lengths of the targets of size `(N)`, where `N = batch size` (must each be <= `S`).
    *  Lengths are specified for each sequence to achieve masking under the assumption that
    *  sequences are padded to equal lengths.
    */
-  const array& target_lengths,
+  const mx::array& target_lengths,
 
   uint64_t blank = 0,   // Blank label, default `0`.
   StreamOrDevice s = {} // Stream on which to schedule the operation
 );
 
-class CTCLoss : public Primitive {
+class CTCLoss : public mx::Primitive {
 private:
   uint64_t blank_;
 public:
-  explicit CTCLoss(Stream stream, uint64_t blank = 0) : Primitive(stream), blank_(blank) {};
-  void eval_cpu(const std::vector<array>& inputs, std::vector<array>& out) override;
-  void eval_gpu(const std::vector<array>& inputs, std::vector<array>& out) override;
+  explicit CTCLoss(mx::Stream stream, uint64_t blank = 0) : mx::Primitive(stream), blank_(blank) {};
+  void eval_cpu(const std::vector<mx::array>& inputs, std::vector<mx::array>& out) override;
+  void eval_gpu(const std::vector<mx::array>& inputs, std::vector<mx::array>& out) override;
   const char* name() const override { return "CTCLoss"; }
-  bool is_equivalent(const Primitive& other) const override {
+  bool is_equivalent(const mx::Primitive& other) const override {
     return static_cast<const CTCLoss&>(other).blank_ == blank_;
   }
 
-  std::vector<array> vjp(
-      const std::vector<array>& primals,
-      const std::vector<array>& cotangents,
+  std::vector<mx::array> vjp(
+      const std::vector<mx::array>& primals,
+      const std::vector<mx::array>& cotangents,
       const std::vector<int>& argnums,
-      const std::vector<array>& outputs) override;
+      const std::vector<mx::array>& outputs) override;
 };
 
-class CTCLossVJP : public Primitive {
+class CTCLossVJP : public mx::Primitive {
 private:
   uint64_t blank_;
 public:
-  explicit CTCLossVJP(Stream stream, uint64_t blank = 0) : Primitive(stream), blank_(blank) {};
-  void eval_cpu(const std::vector<array>& inputs, std::vector<array>& out) override;
-  void eval_gpu(const std::vector<array>& inputs, std::vector<array>& out) override;
+  explicit CTCLossVJP(mx::Stream stream, uint64_t blank = 0) : mx::Primitive(stream), blank_(blank) {};
+  void eval_cpu(const std::vector<mx::array>& inputs, std::vector<mx::array>& out) override;
+  void eval_gpu(const std::vector<mx::array>& inputs, std::vector<mx::array>& out) override;
   const char* name() const override { return "CTCLossVJP"; }
-  bool is_equivalent(const Primitive& other) const override {
+  bool is_equivalent(const mx::Primitive& other) const override {
     return static_cast<const CTCLossVJP&>(other).blank_ == blank_;
   }
 };
